30a459a20336fc769b5149e622c3ac12a690e6e0
Ramon <ramonrune@gmail.com>
2019-04-20 12:23:01 -0300

    modulo pessoas

---
+++ src/app/pessoas/pessoas.module.ts
@@ -0,0 +1,33 @@
+import { InputMaskModule } from 'primeng/components/inputmask/inputmask';
+import { TooltipModule } from 'primeng/components/tooltip/tooltip';
+import { DataTableModule } from 'primeng/components/datatable/datatable';
+import { ButtonModule } from 'primeng/components/button/button';
+import { InputTextModule } from 'primeng/components/inputtext/inputtext';
+import { FormsModule } from '@angular/forms';
+import { PessoasGridComponent } from './pessoas-grid/pessoas-grid.component';
+import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
+import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    InputTextModule,
+    ButtonModule,
+    DataTableModule,
+    TooltipModule,
+    InputMaskModule,
+  ],
+  declarations: [
+    PessoasPesquisaComponent,
+    PessoaCadastroComponent,
+    PessoasGridComponent
+  ],
+  exports: [
+    PessoaCadastroComponent,
+    PessoasPesquisaComponent
+  ]
+})
+export class PessoasModule { }
