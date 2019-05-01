082a54a60739d73e2f8dc0d1b55a086f132e970a
Ramon <ramonrune@gmail.com>
2019-05-01 20:29:33 -0300

    pesquisa por data de vencimento

--- src/app/lancamentos/lancamento.service.ts
+++ src/app/lancamentos/lancamento.service.ts
@@ -3,10 +3,13 @@ import { Http, Headers } from '@angular/http';
 import { Injectable } from '@angular/core';
 import { URLSearchParams} from '@angular/http';
 import 'rxjs/add/operator/toPromise';
+import * as moment from 'moment';
 
 export interface LancamentoFiltro {
 
   descricao: string;
+  dataVencimentoInicio: Date;
+  dataVencimentoFim: Date;
 
 }
 
@@ -25,6 +28,14 @@ export class LancamentoService {
 
     if (filtro.descricao) {
       params.set('descricao', filtro.descricao);
+    }
+
+    if (filtro.dataVencimentoInicio) {
+      params.set('dataVencimentoDe', moment(filtro.dataVencimentoInicio).format('YYYY-MM-DD'));
+    }
+
+    if (filtro.dataVencimentoFim) {
+      params.set('dataVencimentoAte', moment(filtro.dataVencimentoFim).format('YYYY-MM-DD'));
     }
 
     return this.http.get(`${this.lancamentosUrl}?resumo`,
