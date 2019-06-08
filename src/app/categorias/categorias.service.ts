46c544aab1a64d4d81b1a1252f2daaf94db4be39
Ramon <ramonrune@gmail.com>
2019-06-08 17:57:32 -0300

    ambientes

--- src/app/categorias/categorias.service.ts
+++ src/app/categorias/categorias.service.ts
@@ -1,3 +1,4 @@
+import { environment } from './../../environments/environment';
 import { AuthHttp } from 'angular2-jwt';
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/toPromise';
@@ -5,9 +6,12 @@ import 'rxjs/add/operator/toPromise';
 @Injectable()
 export class CategoriasService {
 
-  categoriasUrl = 'http://localhost:8080/categorias'
+  categoriasUrl: string
 
-  constructor(private http: AuthHttp) { }
+  constructor(private http: AuthHttp) {
+    this.categoriasUrl = `${environment.apiUrl}/categorias`;
+
+  }
 
   listarTodas(): Promise<any> {
     return this.http.get(this.categoriasUrl).toPromise().then(response => response.json());
--- src/app/lancamentos/lancamento.service.ts
+++ src/app/lancamentos/lancamento.service.ts
@@ -1,3 +1,4 @@
+import { environment } from './../../environments/environment';
 import { AuthHttp } from 'angular2-jwt';
 import { Lancamento } from './../core/model';
 
@@ -19,9 +20,11 @@ export class LancamentoFiltro {
 @Injectable()
 export class LancamentoService {
 
-  lancamentosUrl = 'http://localhost:8080/lancamentos';
+  lancamentosUrl: string;
 
-  constructor(private http: AuthHttp) { }
+  constructor(private http: AuthHttp) {
+    this.lancamentosUrl = `${environment.apiUrl}/lancamentos`;
+   }
 
   pesquisar(filtro: LancamentoFiltro): Promise<any> {
     const params = new URLSearchParams();
