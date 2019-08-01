6bd3685d28aa64f9baa2df5bb8e05df9e8e1633d
Ramon <ramonrune@gmail.com>
2019-07-31 21:12:51 -0300

    relatorios

---
+++ src/app/relatorios/relatorios.module.ts
@@ -0,0 +1,16 @@
+import { SharedModule } from './../shared/shared.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RelatoriosRoutingModule } from './relatorios-routing.module';
+import { RelatorioLancamentosComponent } from './relatorio-lancamentos/relatorio-lancamentos.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    SharedModule,
+    RelatoriosRoutingModule
+  ],
+  declarations: [RelatorioLancamentosComponent]
+})
+export class RelatoriosModule { }
